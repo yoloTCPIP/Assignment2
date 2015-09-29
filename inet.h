7250f0219c0e1c67e49c2d46d191a16af50ef99f
yoloTCPIP <callmedeath94@hotmail.com>
2015-09-29 22:14:30 +0800

    Create inet.h

---
+++ inet.h
@@ -0,0 +1,19 @@
+#include <string.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <dirent.h>
+#include <ctype.h>
+#include <limits.h>
+#include <stddef.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <sys/wait.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#define SERV_TCP_PORT 25000
+#define CLADDR_LEN 30
